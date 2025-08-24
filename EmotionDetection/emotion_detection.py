41986f877879f62659d2160d770fc0a20f3fd2b9
Eliseo Castro <eliseo.castro.v@gmail.com>
2025-08-23 22:55:32 -0400

    Initial commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    request_body = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = request_body, headers = headers)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+
+    emotions_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotions_scores, key=emotions_scores.get)
+    emotions_scores['dominant_emotion'] = dominant_emotion
+    return emotions_scores
---
+++ server.py
@@ -0,0 +1,30 @@
+"""
+    Emotion Detector docstring
+"""
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask('emotionDetector')
+
+@app.route("/emotionDetector")
+def emotion_analyzer():
+    """
+        Docstring
+    """
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+    dominant_emotion = response['dominant_emotion']
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    return f"The system response is {response}.The dominant emotion is {dominant_emotion}"
+
+@app.route("/")
+def render_index_page():
+    """
+        Docstring
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
